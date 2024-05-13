eb8ee981891585683b42b8daf8b95fba85902d9d
Ramsey Louder <RamseyLouder@gmail.com>
2024-05-13 09:02:12 -0400

    first commit

---
+++ CircleObjects/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    public class Circle
+    {
+        //properties
+        private double Radius { get; set; }
+       
+        //public Diameter {get;set;}
+
+
+        //constructors
+        public Circle(double radius)
+        {
+            Radius = radius;
+            
+       
+
+        }
+
+        //methods
+        public  double CalculateDiameter()
+        {
+            
+            double diameter = 0;
+            diameter = 2 * (Radius);
+            return diameter;
+        }
+        public double CalculateCircumfrence()
+        {
+           
+            double circumfrence = 0;
+            circumfrence = 2 * (Math.PI * Radius);
+            return circumfrence;
+            
+        }
+        public double CalculateArea()
+        {
+            double area = 0;
+            area = Math.PI* (Math.Pow(Radius,2));
+            return area;
+        }
+        public void Grow()// we need it to perform a function but we don't need it to display
+        {
+           double growradius = 0;
+            growradius = 2 * (Radius);
+            
+            
+        }
+
+        public double GetRadius()
+        {
+            double finalRadius = 0;
+            for (int i =0; i< Radius; i++)
+            {
+                finalRadius += i;
+            }
+            return finalRadius;
+        }
+    }
+}
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CircleObjects;
+
+
+
+
+
+
+
+
+Circle NewCircle = null;
+bool repeat = true;
+while (repeat == true)
+{
+
+    Console.WriteLine("Please enter the radius of a circle.");
+    bool validate = true;
+    while (validate == true)
+    {
+        double radiusInput = double.Parse(Console.ReadLine());
+         NewCircle = new Circle(radiusInput); //this will take the user input for radius and send it to the circle constructor.
+
+        if (radiusInput <= 0)
+        {
+
+            Console.WriteLine("Invalid input. Please enter a positive number.");
+            validate = true;
+
+        }
+
+        else
+        {
+            
+            NewCircle.CalculateDiameter();
+            Console.WriteLine($"Diameter:{NewCircle.CalculateDiameter()}   Circumfrence: {NewCircle.CalculateCircumfrence()}   Area:{NewCircle.CalculateArea()}");
+            validate = false;
+        }
+    }
+        Console.WriteLine("Would you like the circle to grow?y/n");
+        string grow = Console.ReadLine();
+        if (grow == "n")
+        {
+        
+            Console.WriteLine($"The radius of the circle is {NewCircle.GetRadius()} Goodbye!");
+            repeat = false;
+            break;
+        }
+        else
+        {
+            NewCircle.Grow();
+        
+            repeat = true;
+        }
+
+    }
+
+
+
